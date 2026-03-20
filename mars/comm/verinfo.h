e9fddc0dfb2968e26952f80f18230f21304ede02
jobsygao <jobsygao@tencent.com>
2026-03-20 10:08:02 +0800

    chore: update version information and add ignored directories

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,11 +2,11 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "683e3291"
+#define MARS_REVISION "e2e7f691"
 #define MARS_PATH "build"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2026-03-17 14:17:13"
+#define MARS_BUILD_TIME "2026-03-17 16:58:29"
 #define MARS_TAG ""
-#define MARS_BUILD_TIMESTAMP 1773728233
+#define MARS_BUILD_TIMESTAMP 1773737909
 
 #endif
