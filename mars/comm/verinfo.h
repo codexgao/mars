bc6f1d638e9df36660fda0268c560d746d08cdcc
jobsygao <jobsygao@tencent.com>
2026-03-22 13:00:31 +0800

    chore: update verinfo.h and Podfile.lock after macOS XCFramework rebuild
    Auto-generated: verinfo.h reflects new build timestamp/revision,
    Podfile.lock reflects updated xlog_flutter podspec checksum.

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,11 +2,11 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "73254a04"
+#define MARS_REVISION "5fa325c0"
 #define MARS_PATH "build"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2026-03-21 18:00:27"
+#define MARS_BUILD_TIME "2026-03-22 12:48:48"
 #define MARS_TAG ""
-#define MARS_BUILD_TIMESTAMP 1774087227
+#define MARS_BUILD_TIMESTAMP 1774154928
 
 #endif
