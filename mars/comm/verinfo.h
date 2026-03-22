e6f9dee188194b6fbd00a3820cba211638f48cfb
jobsygao <jobsygao@tencent.com>
2026-03-22 13:14:44 +0800

    chore: update verinfo.h after Windows Debug build

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,11 +2,11 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "5fa325c0"
+#define MARS_REVISION "bc6f1d63"
 #define MARS_PATH "build"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2026-03-22 12:48:48"
+#define MARS_BUILD_TIME "2026-03-22 13:13:01"
 #define MARS_TAG ""
-#define MARS_BUILD_TIMESTAMP 1774154928
+#define MARS_BUILD_TIMESTAMP 1774156381
 
 #endif
