e41380910858c02a81de0918896471d48e0d5091
jobsygao <jobsygao@tencent.com>
2026-03-25 18:43:56 +0800

    feat(example/ios): add native XLog ObjC wrapper usage in AppDelegate

--- samples/xlog_flutter/example/ios/Runner/AppDelegate.swift
+++ samples/xlog_flutter/example/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import xlog_flutter
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,30 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    // MARK: - xlog native ObjC wrapper usage example
+    let docsDir = NSSearchPathForDirectoriesInDomains(
+        .documentDirectory, .userDomainMask, true
+    ).first!
+    let logDir = (docsDir as NSString).appendingPathComponent("xlog_native")
+
+    let config = XLogConfig()
+    config.logdir      = logDir
+    config.nameprefix  = "native_demo"
+    config.mode        = .async
+    config.compressMode = .zlib
+
+    if let log = XLogManager.open(with: config, level: .debug) {
+      log.info("AppDelegate", msg: "xlog native ObjC wrapper initialized")
+      log.debug("AppDelegate", msg: "application did finish launching")
+      log.warn("AppDelegate", msg: "this is a warning from native wrapper")
+
+      // Flush and release when done
+      log.flush(false)
+      XLogManager.releaseInstance(withName: "native_demo")
+    }
+    // END xlog example
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
